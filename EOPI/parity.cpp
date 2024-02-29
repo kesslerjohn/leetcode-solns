2352020c32d2c32744b896f7ce0554133b6442f6
kesslerjohn <john-kessler@uiowa.edu>
2024-02-29 13:40:37 -0600

    complete linear bit counting function

--- EOPI/parity.cpp
+++ EOPI/parity.cpp
@@ -3,10 +3,19 @@ using namespace std;
 
 class Solution {
 public:
-    int parity(int n){
-        int x = n & ~(n-1);
-        return x;
+    int bitCount(int x){
+        int c = 0;
+        while (x > 0){
+            int bit = x & ~(x-1); // find the smallest bit set to '1'
+            x = x - bit; // subtract it from x
+            c += 1; // count how many times you do that
+        }
+        return c;
     };
+
+    int parity(int x){
+        return 0;
+    }
 };
 
 int main(int argc, char* argv[]){
@@ -14,6 +23,9 @@ int main(int argc, char* argv[]){
         cout << "too few arguments" << endl;
         return 1;
     }
-    cout << argv[1] << endl;
+    int x = stoi(argv[1]);
+    Solution sol = Solution();
+    cout << "Parity: " << sol.parity(x) << endl;
+    cout << "Count: " << sol.bitCount(x) << endl;
     return 0;
 }
