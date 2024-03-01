bd5ce6fd7d8c46b04cb61ce07d23b0c4d1b3df00
kesslerjohn <john-kessler@uiowa.edu>
2024-02-29 20:44:13 -0600

    start swapbits

---
+++ EOPI/swap_bits.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution{
+public:
+    int swapBits(int x){
+        return 0;
+    }
+
+}
+
+int main(int argc, char* argv[]){
+    if (argc < 2){
+        cout << "too few arguments" << endl;
+        return 1;
+    }
+}
