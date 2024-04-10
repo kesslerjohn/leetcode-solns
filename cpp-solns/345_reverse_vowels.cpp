fe0b6aa96a2b36f4fc73ca22498afce4a31dd356
kesslerjohn <john-kessler@uiowa.edu>
2024-04-10 16:20:24 -0500

    complete 345

--- cpp-solns/345_reverse_vowels.cpp
+++ cpp-solns/345_reverse_vowels.cpp
@@ -6,8 +6,26 @@ using namespace std;
 
 class Solution{
 public:
-    string vowelsReversed(string s){
-        return "";
+    string reverseVowels(string s) {
+        regex vwl ("[aeiouAEIOU]");
+        smatch temp;
+        if (!regex_search(s, temp, vwl)){
+            return s;
+        }
+        string vowel = "aeiouAEIOU";
+        string s_p = s;
+        char temp_s;
+        reverse(s.begin(), s.end());
+        int head = s_p.find_first_of(vowel);
+        int tail = s_p.find_last_of(vowel);
+        while (head < tail){
+            temp_s = s_p[head];
+            s_p[head] = s_p[tail];
+            s_p[tail] = temp_s;
+            head = s_p.find_first_of(vowel, head + 1);
+            tail = s_p.find_last_of(vowel, tail - 1);
+        }
+        return s_p;
     }
 };
 
@@ -15,7 +33,7 @@ int main(){
     regex vowel ("[aeiouAEIOU]");
     string userin;
     getline(cin, userin);
-    string formatted = regex_replace(userin, vowel, "{}");
+    string formatted = reverseVowels(userin);
     cout << formatted << endl;
     return 0;
 }
