662db9f275b7152121c6cfed599235844c43370e
kesslerjohn <john-kessler@uiowa.edu>
2024-04-10 10:32:23 -0500

    Start 605

---
+++ cpp-solns/605_can_place_flowers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution{
+public:
+    bool canPlaceFlowers(vector<int> &flowerbed, int n){
+        return true;
+    }
+
+};
