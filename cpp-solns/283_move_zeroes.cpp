522d31ad65dc2351b1b84b2aeddf8f25b4c674ab
kesslerjohn <john-kessler@uiowa.edu>
2024-04-10 21:13:19 -0500

    complete 283

--- cpp-solns/283_move_zeroes.cpp
+++ cpp-solns/283_move_zeroes.cpp
@@ -5,7 +5,22 @@ using namespace std;
 
 class Solution{
 public:
-    void moveZeroes(){
-        cout << "Yayyyy :-)" << endl;
+    // iterate over the array
+    // keep a pointer to the next space that needs a non-zero
+    // number filled in. When you encounter a non-zero
+    // number, move it to the pointer and increment the pointer by 1
+    // at the end of the array, fill from pointer+1 to the end with 0s.
+    void moveZeroes(vector<int>& nums){
+        int ptr = 0;
+        
+        for (int n : nums){
+            if (n != 0){
+                nums[ptr] = n;
+                ptr++;
+            }
+        }
+        for (int i = ptr; i < nums.size(); i++){
+            nums[i] = 0;
+        }
     }
 };
