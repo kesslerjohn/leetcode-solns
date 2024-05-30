29acc613f95c6259948c27b138bc6ece1d647ba5
kesslerjohn <john-kessler@uiowa.edu>
2024-05-30 09:36:04 -0500

    add slower solution to product of arr except self

--- cpp-solns/238_product_of_arr_except_self.cpp
+++ cpp-solns/238_product_of_arr_except_self.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
+class Solution {
+public:
+    // this solution is O(N^2) and gets time limit exceeded
+    vector<int> slowProductExceptSelf(vector<int>& nums) { 
+        vector<int> answer(nums.size());
+
+        for (int i = 0; i < nums.size(); i++){
+            answer[i] = std::accumulate(nums.begin()+1, nums.end(), 1, std::multiplies<int>());
+            std::rotate(nums.begin(), nums.begin()+1, nums.end());
+        }
+        return answer;
+    }
+};
+
 int main(){
     return 0;
 }
