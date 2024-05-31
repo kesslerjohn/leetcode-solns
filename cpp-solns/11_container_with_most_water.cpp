54d1043e1c0df817dfe95d7abe5de536cf18743e
kesslerjohn <john-kessler@uiowa.edu>
2024-05-31 13:34:21 -0500

    faster c++ solution to 11

--- cpp-solns/11_container_with_most_water.cpp
+++ cpp-solns/11_container_with_most_water.cpp
@@ -5,48 +5,23 @@ using namespace std;
 
 class Solution {
 public:
-
-    int area(vector<int> height, int i, int j) {
-        return (j-i)*min(height[i], height[j]);
-    }
-
-    int mod(int k, int n) {
-        return ((k %= n) < 0) ? k+n : k;
-    }
-
     int maxArea(vector<int>& height) {
-        // find i, j st:
-        // j > i;
-        // max (j - i)*min(height[i], height[j]);
+        int left = 0;
+        int right = height.size()-1;
+        int maxArea, currentArea;
 
-        int sz = height.size();
-        int a = 0;
-        int row = 1;
-        int mid_max = 0;
+        while (left < right) {
+            currentArea = (right - left)*min(height[left], height[right]);
+            maxArea = max(maxArea, currentArea);
 
-        vector<int> col(sz);
-        vector<vector<int>> dp_arr(sz);
-
-        // simple area function for readability
-
-        // initialize vector for DP
-        for (int i = 0; i < sz; i++){
-            dp_arr[i] = col;
-            dp_arr[i][mod(i-1, sz)] = area(height, mod(i-1, sz), i);
-        }
-
-        for (int j = 2; j < sz; j++) {
-            mid_max = 0;
-            for (int k = 0; k < j; k++) {
-                a = area(height, k, j);
-                if (a > mid_max) {
-                    mid_max = a;
-                }
+            if (height[left] < height[right]){
+                left++;
+            } else {
+                right--;
             }
-            dp_arr[j][0] = max(max(dp_arr[j-1][0], dp_arr[j][row]), mid_max);
-            row++;
         }
-        return dp_arr[sz-1][0];
+        
+        return maxArea;
     }
 };
 
