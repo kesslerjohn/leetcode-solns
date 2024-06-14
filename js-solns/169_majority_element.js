becb131dab46f1b2f1bbf4e8be8d078ca5ac6e87
kesslerjohn <john-kessler@uiowa.edu>
2024-06-14 10:32:39 -0500

    add constant space solution to 169: majority element

--- js-solns/169_majority_element.js
+++ js-solns/169_majority_element.js
@@ -12,3 +12,24 @@ var majorityElement = function(nums) {
     })
     return res
 };
+
+var majorityElementCSpace = function(nums) {
+    nums.sort((a, b) => {return a - b})
+    const maj = Math.floor(nums.length/2)
+    let prev = nums[0]
+    let count = 1
+    let res = prev
+    for (let i = 1; i < nums.length; i++){
+        if (nums[i] === prev){
+            count++
+            if (count > maj){
+                res = nums[i]
+                return res
+            }
+        } else {
+            prev = nums[i]
+            count = 1
+        }
+    }
+    return res
+}
