bc86c0d0975e6b8f90a960a097d96e0173c61ce7
kesslerjohn <john-kessler@uiowa.edu>
2024-06-26 07:54:23 -0500

    add faster js solution to twosum

--- js-solns/1_two_sum.js
+++ js-solns/1_two_sum.js
@@ -1,3 +1,14 @@
+var twoSumFast = function(nums, target) {
+    let mem = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (mem.has(complement)) {
+            return [mem.get(complement), i];
+        }
+        mem.set(nums[i], i)
+    }
+}
+
 
 // this slower solution brute forces the prob,
 // and does not get TLE on leetcode
